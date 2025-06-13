ca755cd5a7e45753ade9e9f7b1d65873b0aaec65
Olivier Banville <olivier@banville.net>
2025-06-13 14:55:52 -0400

    add base model

--- apps/ontologies/admin.py
+++ apps/ontologies/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Ontology)
+class OntologyAdmin(admin.ModelAdmin):
+    list_display = ("label", "uri", "created_at")
+    search_fields = ("label", "uri")
+    ordering = ("-created_at",)
+
+
+@admin.register(models.Term)
+class TermAdmin(admin.ModelAdmin):
+    list_display = ("label", "ontology", "is_favorite", "weight", "created_at")
+    search_fields = ("label", "uri")
+    ordering = ("uri",)
+    list_filter = ("ontology", "is_favorite")
--- apps/ontologies/models.py
+++ apps/ontologies/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    weight = models.FloatField(default=1.0, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Ontology(BaseModel):
+    uri = models.URLField(primary_key=True)
+    label = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.label or self.uri
+
+
+class Term(BaseModel):
+    uri = models.URLField(primary_key=True)
+    ontology = models.ForeignKey(
+        Ontology,
+        related_name="terms",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+    label = models.CharField(max_length=255, blank=True, null=True)
+    definition = models.TextField(blank=True, null=True)
+    subClassOf = models.ForeignKey(
+        "self",
+        related_name="subClasses",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+    weight = models.FloatField(default=1.0, blank=True, null=True)
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.label or self.uri
